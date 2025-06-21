95e8606c51e1bb476286cc1aa8a92f55fd2eb43d
Sleax-hub <sleaxacademy@gmail.com>
2025-06-21 19:59:01 +0300

    first home tab modification

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,4 +1,4 @@
-import { Image, Pressable, ScrollView, Text, View } from "react-native";
+import { Image, Pressable, ScrollView, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import Ionicons from '@expo/vector-icons/Ionicons';
@@ -13,7 +13,7 @@ export default function Index() {
             <Ionicons name="menu" size={28} color="black" />
         </View>
         <View>
-          <Text className="font-bold text-2xl text-purple-800">MOBIA WORLD</Text>
+          <Text className="font-bold text-2xl text-primary">MOBIA WORLD</Text>
         </View>
         <View>
           <Ionicons name="notifications-outline" size={28} color="black" />
@@ -21,15 +21,32 @@ export default function Index() {
       </View>
 
       {/* ads preview */}
-        <View className="px-4 mt-4">
-          <Image source={require('../../assets/images/424.jpg')} className="w-full h-40 rounded-2xl " resizeMode="cover"/>
+        <View className="px-4 mt-2">
+          <Image source={require('../../assets/images/9811920.jpg')} className="w-full h-52 rounded-2xl " resizeMode="cover"/>
           
-          <View className="bg-yellow-400 w-20 px-2 rounded-xl flex items-center justify-center">
+          <View className="bg-fuchsia-500 w-26 h-10 px-2 rounded-2xl flex items-center justify-center absolute bottom-4 right-12">
             <Pressable>
-              <Text>Shop Now</Text>
+              <Text className="font-bold tracking-tight text-white">Shop Now</Text>
             </Pressable>
           </View>
         </View>
+
+        {/* search bar */}
+        <View className="px-4 mt-4 flex-col w-full">
+          <View>
+            <TextInput placeholder="Find items here...." className="border rounded-xl border-gray-300 placeholder:text-gray-400 h-14 focus:border-secondary "/>
+          </View>
+          <View className="absolute top-1 right-5 bg-primary rounded-full p-2">
+            <Ionicons name="search-sharp" size={28} color="white" />
+          </View>
+        </View>
+
+        {/* categories */}
+
+        <View className="px-4 mt-4">
+          <Text className="text-2xl font-bold text-primary">Categories</Text>
+        </View>
+
 
 
       <View className="flex-1 px-4 mt-10">
---
+++ components/categories.js
@@ -0,0 +1,3 @@
+const caterogies = {
+    NOKIA,
+}
